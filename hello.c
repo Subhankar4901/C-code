9d7c1357a1ef42d32de40fcf6f29d3315f916ca0
Subhankar4901 <Subhankarh701@gmail.com>
2020-12-02 01:34:50 +0530

    Add a new line to hello.c

--- hello.c
+++ hello.c
@@ -4,7 +4,8 @@ int main()
  printf("Hello\n");
  printf("Just trying git\n");
  printf("Just checking\n");
- printf("Again just checking\n);
+ printf("Again just checking\n");
  printf("Doing stuff from different branch\n");
+ printf("Just playing with git rebase");
 return 0;
 }
